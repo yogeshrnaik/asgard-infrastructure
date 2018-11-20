1640d809d027c8423ac3a8ca538a92f601173d67
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-11-20 19:07:42 +0530

    Added user data for instand registration.

--- exercise2/main.tf
+++ exercise2/main.tf
@@ -12,6 +12,14 @@ resource "aws_instance" "cluster-ec2-instance" {
   iam_instance_profile = "${aws_iam_instance_profile.ecs_cluster_instance_profile.name}"
   security_groups = ["${aws_security_group.ecs-instance-security.id}"]
 
+  user_data = <<EOF
+    cat <<'CONFIG' >> /etc/ecs/ecs.config
+    ECS_CLUSTER=${aws_ecs_cluster.ecs-cluster.name}
+    ECS_ENABLE_TASK_IAM_ROLE=true
+    ECS_ENABLE_TASK_IAM_ROLE_NETWORK_HOST=true
+    CONFIG
+  EOF
+
   root_block_device {
     volume_size = "30"
     delete_on_termination = true
